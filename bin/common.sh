e07f4e2e131e47427811980a37f52d3256e6d99a
Dave Carroll <djcarroll+git@gmail.com>
2014-11-17 15:46:24 -0800

    debuggin

--- bin/common.sh
+++ bin/common.sh
@@ -43,8 +43,8 @@ export_env_dir() {
 }
 
 type setopt >/dev/null 2>&1 && setopt shwordsplit
-PLATFORMS="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64"
-#PLATFORMS="darwin/amd64"
+#PLATFORMS="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64"
+PLATFORMS="darwin/amd64"
 
 
 function go-alias {
