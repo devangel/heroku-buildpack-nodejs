b3a91905602d797cb630e5ff14d2bbbeba27cd15
Dave Carroll <djcarroll+git@gmail.com>
2014-11-19 11:28:59 -0800

    sym link fix for aws

--- bin/common.sh
+++ bin/common.sh
@@ -19,7 +19,7 @@ install_aws() {
   cd $1
   wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -nv
   unzip awscli-bundle.zip
-  ./awscli-bundle/install -b $1/vendor/bin/aws -i $1/vendor/lib/aws
+  ./awscli-bundle/install -i $1/vendor/lib/aws
   rm awscli-bundle.zip
   rm -rf awscli-bundle
 }
